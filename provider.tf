0b8830f5042705e0fb71c2e60cc01b21551fe8e2
Punitojha <ojha.punit07@mail.com>
2025-05-11 22:10:10 +0530

    code added

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.13.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+subscription_id = "afd16d4c-a9a7-4226-a637-b843d1e25f1e"
+}
