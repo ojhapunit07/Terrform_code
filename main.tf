8bed1c168039c009bdea273ec55b0e9b25292f12
ojhapunit07 <164520758+ojhapunit07@users.noreply.github.com>
2025-09-03 22:03:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 resource "azurerm_resouce_group" "rg1" {
   name     = "my-rg"
   location = "central india"
+}
 
+resource "azurerm_storage_account" "st" {
+name = "myst01"
+location = "central india"
+resource_group = "my_rg"
+account_tier             = "Standard"
+account_replication_type = "GRS"
 }
 
